be04241be315a503e5da2be39513f5d50e33c231
redstoone <cy_jing_2010@163.com>
2018-03-16 22:34:16 +0800

    init

---
+++ pages/classify/index.js
@@ -0,0 +1,14 @@
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    
+  },
+  
+})
