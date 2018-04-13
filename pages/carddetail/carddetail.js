f66fb158c64c4ebc643cb99cffca7f8a4ac24ca0
SJW <398226243@qq.com>
2018-04-13 17:42:19 +0800

    4.13.17

--- pages/carddetail/carddetail.js
+++ pages/carddetail/carddetail.js
@@ -6,9 +6,9 @@ Page({
    */
   data: {
     list:[
-      { name: '活动名称', place: '输入活动标题', isInp: true, value:'' },
-      { name: '活动名称', place: '输入活动标题', isInp: false, value: '' },
-      { name: '活动名称', place: '输入活动标题', isInp: false, value: '' },
+      { name: '活动名称', place: '输入活动标题', isInp: true, value:'',id:0,txtnum:0},
+      { name: '活动名称', place: '输入活动标题', isInp: false, value: '', id: 1, txtnum: 0},
+      { name: '活动名称', place: '输入活动标题', isInp: false, value: '', id: 2, txtnum: 0},
     ]
   },
 
@@ -16,6 +16,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   txtChange(e){
-    console.log(e)
+    var that = this
+    var idx = Number(e.currentTarget.dataset.index);
+    if (idx == 0){
+      that.setData({
+        'list[0].txtnum': e.detail.value.length
+      })           
+    }
+    if (idx == 1) {
+      that.setData({
+        'list[1].txtnum': e.detail.value.length
+      })
+    }
+    if (idx == 2) {
+      that.setData({
+        'list[2].txtnum': e.detail.value.length
+      })
+    }   
   }
 })
