de59069e05a285bb2d3f0e69c7e21b2a7ef84ee6
shenjunwei321 <398226243@qq.com>
2018-04-14 19:23:55 +0800

    s

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -5,62 +5,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
-  },
+    week:''
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    getWeek:function(week){
+    if (week>=0&&week<7){
+      switch(week){
+        case 0:{
+          return "周日";
+        }
+        case 1: {
+          return "周一";
+        }
+        case 2: {
+          return "周二";
+        }
+        case 3: {
+          return "周三";
+        }
+        case 4: {
+          return "周四";
+        }
+        case 5: {
+          return "周五";
+        }
+        case 6: {
+          return "周六";
+        }
+      }
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  getDateList: function () {
+    var time = new Date();
+    var year = time.getFullYear();
+    var month = time.getMonth() + 1;
+    var day = time.getDate();
+    var week = time.getDay();
+    var today = month + "/" + day;
+    //当月天数
+    var count = new Date(year, month, 0).getDate();
+    var list = [];
+    var newList = [[]];
+    for (var i = 0; i < 7; i++) {
+      var _week = (week + i) % 7;//星期
+      var str = (i == 0 ? "今天" : (i == 1 ? "明天" : (i == 2 ? "后天" : this.getWeek(_week))));
+      var _month = month + parseInt((day + i) / count);//月份
+      var _count = new Date(year, (month + 1), 0).getDate();//当月天数
+      var _day = 0;
+      if ((day + i >= count + _count) || parseInt((day + i) / count) == 2) {
+        _month = month + 1 + parseInt((day + i - count) / _count);
+        _day = (day + i - count) % _count;
+      } else {
+        _day = (day + i) % count;//日期
+        _count = 0;
+      }
+      if (_day == 0) {
+        _month -= 1;
+        _day = _count || count;
+      }
+      if (_month - month != newList.length - 1) {
+        newList.push([]);
+      }
+      newList[_month - month].push({ str: str.replace(/周[\s|\S]$/, ''), year: year, month: _month, week: _week, day: (_day < 10 ? "0" : "") + _day, isDate: true });
+    }
+    console.log(newList)
+    newList[0][0].day = "今天"
+    this.setData({
+      week: week,
+      years: newList
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  clickChecked(e){
+    console.log(e)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  onLoad: function () {
+    this.getDateList();
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
