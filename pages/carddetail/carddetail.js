ad3ff32f6866272767d31be58dc463f6b3753f6c
shenjunwei321 <398226243@qq.com>
2018-04-15 14:29:26 +0800

    s

--- pages/carddetail/carddetail.js
+++ pages/carddetail/carddetail.js
@@ -1,4 +1,5 @@
 // pages/carddetail/carddetail.js
+const app = getApp()
 Page({
 
   /**
@@ -6,10 +7,12 @@ Page({
    */
   data: {
     list:[
-      { name: '活动名称', place: '输入活动标题', isInp: true, value:'',id:0,txtnum:0},
-      { name: '活动名称', place: '输入活动标题', isInp: false, value: '', id: 1, txtnum: 0},
-      { name: '活动名称', place: '输入活动标题', isInp: false, value: '', id: 2, txtnum: 0},
-    ]
+      { name: '活动名称', place: '请输入活动名称', isInp: true, value:'',id:0,txtnum:0},
+      { name: '群主简介', place: '请输入群主简介', isInp: false, value: '', id: 1, txtnum: 0},
+      { name: '打卡公告', place: '请输入打卡公告', isInp: false, value: '', id: 2, txtnum: 0},
+    ],
+    wxtxt:'',
+    id:''
   },
 
   /**
@@ -20,18 +23,79 @@ Page({
     var idx = Number(e.currentTarget.dataset.index);
     if (idx == 0){
       that.setData({
-        'list[0].txtnum': e.detail.value.length
+        'list[0].txtnum': e.detail.value.length,
+        'list[0].value': e.detail.value
       })           
     }
     if (idx == 1) {
       that.setData({
-        'list[1].txtnum': e.detail.value.length
+        'list[1].txtnum': e.detail.value.length,
+        'list[1].value': e.detail.value
       })
     }
     if (idx == 2) {
       that.setData({
-        'list[2].txtnum': e.detail.value.length
+        'list[2].txtnum': e.detail.value.length,
+        'list[2].value': e.detail.value
       })
     }   
+  },
+  wxtxt(e){
+    console.log(e.detail.value)
+      var that=this;
+
+      that.setData({
+        wxtxt: e.detail.value
+      })
+  },
+  next() {
+    if (this.data.list[0].value.length == 0) {
+      wx.showToast({
+        title: '请输入活动名称！',
+        icon: 'loading',
+        duration: 1500
+      })
+      return false;
+    }
+    if (this.data.list[1].value.length == 0) {
+      wx.showToast({
+        title: '请输入群主简介！',
+        icon: 'loading',
+        duration: 1500
+      })
+      return false;
+    }
+    if (this.data.list[2].value.length == 0) {
+      wx.showToast({
+        title: '请输入打卡公告！',
+        icon: 'loading',
+        duration: 1500
+      })
+      return false;
+    }
+    if (this.data.wxtxt.length == 0) {
+      wx.showToast({
+        title: '请输入群主微信！',
+        icon: 'loading',
+        duration: 1500
+      })
+      return false;
+    }
+
+    app.postRequest('/wx/activity/detail/add_second', 'POST', { mainDescription: this.data.list[1].value, activityNotice: this.data.list[2].value, mainWx: this.data.wxtxt, consumerId: app.globalData.openid, id: this.data.id }, (res) => {
+        if (res.data.success) {
+          wx.navigateTo({
+            url: '../lookdetail/carddetail',
+          })
+        }
+      })
+  },
+  onLoad(e){
+    var that = this;
+    that.setData({
+      'list[0].txtnum': e.title.length,
+      'list[0].value': e.title,
+      id:e.id
+    })
   }
 })
