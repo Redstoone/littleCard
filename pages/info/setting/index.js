6581e07af40c1763b72e30bb0aeb6c716b17de0f
redstoone <cy_jing_2010@163.com>
2018-04-15 10:14:14 +0800

    修改

--- pages/info/setting/index.js
+++ pages/info/setting/index.js
@@ -1,3 +1,4 @@
+const app = getApp()
 Page({
   data: {
     headicon: 'https://t10.baidu.com/it/u=3788365272,318725089&fm=173&app=25&f=JPEG?w=640&h=363&s=361016CC28B3EA475C13653D0300505A',
@@ -8,7 +9,26 @@ Page({
     gender: 0,
     genderName: '未知',
     phone: '',
-    realname: ''
+    realname: '',
+    brithDate: ''
+  },
+
+  onLoad () {
+    this.getUserinfo()
+  },
+
+  getUserinfo () {
+    app.postRequest('/wx/consumer/record', 'POST', { consumerId: app.globalData.openid}, (res) => {
+      this.setData({
+        user: res.data.user
+      })
+    })
+  },
+
+  bindDateChange: function (e) {
+    this.setData({
+      brithDate: e.detail.value
+    })
   },
 
   camNicknameTxt: function (e) {
@@ -27,6 +47,12 @@ Page({
     var that = this
     that.setData({
       realname: e.detail.value
+    })
+  },
+  camSignTxt: function (e) {
+    var that = this
+    that.setData({
+      sign: e.detail.value
     })
   },
 
@@ -65,5 +91,20 @@ Page({
       })
       return false;
     }
+
+    app.postRequest('/wx/consumer/merged', 'POST', 
+    { 
+      id: app.globalData.openid,
+      nickname: this.globalData.userInfo.nickName,
+      headicon: this.globalData.userInfo.avatarUrl,
+      gender: this.globalData.userInfo.gender,
+      province: this.globalData.userInfo.province,
+      city: this.globalData.userInfo.city,
+      county: this.globalData.userInfo.country
+    }, (res) => {
+      this.setData({
+        user: res.data.user
+      })
+    })
   }
 })
