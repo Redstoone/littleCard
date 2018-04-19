01fe22d72e103c8f35dbb61cb4729e042eab94b9
redstoone <cy_jing_2010@163.com>
2018-04-19 20:21:29 +0800

    修改一波

--- pages/newCard/newCard.js
+++ pages/newCard/newCard.js
@@ -2,28 +2,38 @@
 const app = getApp()
 Page({
   data: {
-    items: [
-      {id:1, value: '1', text: '不限日期，长期打卡', checked: true },
-      {id:2, value: '2', text: '指定开始结束日期', checked: false }
+    items: [{
+        id: 1,
+        value: '1',
+        text: '不限日期，长期打卡',
+        checked: true
+      },
+      {
+        id: 2,
+        value: '2',
+        text: '指定开始结束日期',
+        checked: false
+      }
     ],
-    title:'',//活动标题
-    startTime:'',//开始时间
-    overTime:''//结束时间
+    title: '', //活动标题
+    startTime: '', //开始时间
+    overTime: '', //结束时间
+    isTimeDisabled: true
   },
   radioChange: function (e) {
-    console.log(e)
     var that = this;
-    var items =[];
-    for (var i = 0; i < that.data.items.length;i++){
+    var items = [];
+    for (var i = 0; i < that.data.items.length; i++) {
       var item = that.data.items[i]
-      item.checked = false  
-      if (e.detail.value && e.detail.value === that.data.items[i].value){
-        item.checked =true        
+      item.checked = false
+      if (e.detail.value && e.detail.value === that.data.items[i].value) {
+        item.checked = true
       }
       items.push(item)
     }
     that.setData({
-      items:items
+      items: items,
+      isTimeDisabled: items[0].checked ? true : false
     })
   },
   startTimeChange: function (e) {
@@ -36,13 +46,13 @@ Page({
       overTime: e.detail.value
     })
   },
-  inputTxt(e){
-    var that= this
+  inputTxt(e) {
+    var that = this
     that.setData({
-      title:e.detail.value
+      title: e.detail.value
     })
   },
-  next(){
+  next() {
     if (this.data.title.length == 0) {
       wx.showToast({
         title: '请输入活动名称！',
@@ -51,17 +61,21 @@ Page({
       })
       return false;
     }
-    if (this.data.items[0].checked == true){
-      app.postRequest('/wx/activity/add_first', 'POST', { name: this.data.title, timeType: 10, consumerId: app.globalData.openid }, (res) => {
+    if (this.data.items[0].checked == true) {
+      app.postRequest('/wx/activity/add_first', 'POST', {
+        name: this.data.title,
+        timeType: 10,
+        consumerId: app.globalData.openid
+      }, (res) => {
         if (res.data.success) {
           var title = res.data.item.name;
           var id = res.data.item.id
-            wx.navigateTo({              
-              url: '../carddetail/carddetail?id=' + id + "&title=" + title,
-            })
+          wx.navigateTo({
+            url: '../carddetail/carddetail?id=' + id + "&title=" + title,
+          })
         }
       })
-    }else{
+    } else {
       if (this.data.startTime == '') {
         wx.showToast({
           title: '请选择开始时间！',
@@ -77,8 +91,14 @@ Page({
           duration: 1500
         })
         return false;
-      }      
-      app.postRequest('/wx/activity/add_first', 'POST', { name: this.data.title, timeType: 20, consumerId: app.globalData.openid, startTime: this.data.startTime, overTime: this.data.overTime }, (res) => {
+      }
+      app.postRequest('/wx/activity/add_first', 'POST', {
+        name: this.data.title,
+        timeType: 20,
+        consumerId: app.globalData.openid,
+        startTime: this.data.startTime,
+        overTime: this.data.overTime
+      }, (res) => {
         if (res.data.success) {
           var title = res.data.item.name;
           var id = res.data.item.id
